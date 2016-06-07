c040cfee2fa1acb1c144d6b0a1336ad8e140319c
Jeff Minard <jeff@jrm.cc>
2016-06-06 22:11:12 -0700

    Docker networking article update && dev mode to background

--- dev-mode.sh
+++ dev-mode.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 docker build -t jrmcc .
-docker run -ti --rm --name=jrmccdev -v `pwd`:/app jrmcc
+docker rm -f jrmccdev 2>/dev/null
+# docker run -ti --rm --name=jrmccdev -v `pwd`:/app jrmcc
+docker run -d --name=jrmccdev -v `pwd`:/app jrmcc

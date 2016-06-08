e76e046c1aa29c1078a761efe5c25f8fe0b02870
Jeff Minard <jeff@jrm.cc>
2016-06-07 20:15:38 -0700

    Moved dev file into subdirectory so main repo is static built site

--- dev-mode.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker build -t jrmcc .
-docker rm -f jrmccdev 2>/dev/null
-# docker run -ti --rm --name=jrmccdev -v `pwd`:/app jrmcc
-docker run -d --name=jrmccdev -v `pwd`:/app jrmcc

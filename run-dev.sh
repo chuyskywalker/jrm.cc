fd4cf36b11b0df7d9033ac73e9f123c63063a5db
Jeff Minard <jeff@jrm.cc>
2016-06-07 22:37:00 -0700

    run dev in root

---
+++ run-dev.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR/dev
+
+# Rebuild (if needed, usually cached)
+docker build -t jrmcc .
+
+# Give any existing container a chance to stop gracefully, and then not, and then forcefully
+docker stop jrmccdev
+docker rm jrmccdev 2>/dev/null
+docker rm -f jrmccdev 2>/dev/null
+
+# Start up the latest and greatest
+docker run -d --name=jrmccdev -v `pwd`/../:/app jrmcc

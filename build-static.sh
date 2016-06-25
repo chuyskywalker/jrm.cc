7e91f51de872c7f5802704bc84e91c8427480d3c
Jeff Minard <jeff@jrm.cc>
2016-06-24 21:57:21 -0700

    Nicer formatting

--- build-static.sh
+++ build-static.sh
@@ -10,26 +10,24 @@ cd $DIR
 # Where are we?
 APP_DIR="./deploy/output"
 
-# Remove any existing output
 echo "-- Clean old builds (if present)"
 rm -rf ${APP_DIR}
 
 echo "-- Build static site (hugo)"
 docker run -ti --rm -v `pwd`:/app chuyskywalker/hugo hugo --source="/app/jrmcc" --destination="/app/deploy/output" --baseUrl="http://jrm.cc/"
 
-
 echo "-- Minify the site html"
 docker run -ti --rm -v `pwd`/deploy:/src chuyskywalker/node-html-minifier \
-  find /src/output/ \( -iname "*.html" -or -iname "*htm" \) -exec bash -x -c 'html-minifier --config-file /src/html-minifier-config.json {} > {}.min; mv {}.min {}' \;
+  find /src/output/ \( -iname "*.html" -or -iname "*htm" \) -exec bash -c 'echo " - {}"; html-minifier --config-file /src/html-minifier-config.json {} > {}.min; mv {}.min {}' \;
 
 EXT="css|js|eot|svg|ttf|woff|html|htm"
-echo "Compressing (${EXT}) in ${APP_DIR}"
+echo "-- Compressing (${EXT}) in ${APP_DIR}"
 
 # fetch all source files by ${EXT} (extension) and pre-compress
 find "$APP_DIR" -type f -regextype posix-extended \( -iregex ".*\.(${EXT})$" \) -print0 | while read -d '' sourceFile
 do
     gzip -c9 "${sourceFile}" > "${sourceFile}.gz"
-    echo "Compressed: ${sourceFile} > ${sourceFile}.gz"
+    echo " - ${sourceFile} > ${sourceFile}.gz"
 done
 
 ID=$(git rev-parse --short=12 HEAD)
